5bd3f2f9eba815defbe2a2f6270c5cb4995b1ca4
Mac Klosky <macklosky211@gmail.com>
2025-07-30 00:32:54 -0400

    Fix Level 3 include

---
+++ Scripts/level_3.gd
@@ -0,0 +1,20 @@
+extends Level
+
+func _init() -> void:
+	path = "res://Scenes/level_3.tscn"
+
+func _ready() -> void:
+	dawn = $"Dawn"
+	dusk = $"Dusk"
+	_shift_dimension() # This puts us into DAWN at the start of the level...
+
+func _on_portal_entered(body: Node3D) -> void:
+	if body is not Player: return
+	
+	body.end_level_animation_player.play("CRT_Poweroff")
+	await body.end_level_animation_player.animation_finished
+	get_tree().change_scene_to_file("res://Scenes/MainMenu.tscn")
+
+func _on_lava_entered(body: Node3D) -> void:
+	if body is not Player: return
+	body.reset()
